ec2933785fa4645c35096da0535ea8ca460b83cd
Geofrey Flores <gflores@jahia.com>
2025-09-10 10:17:47 -0400

    Reapply "feat: Drop DC dependency, expose one connection at a time, use ES 9 (#72)" (#75)
    This reverts commit 72e292668c352e7fd7793951a48e9a7727f16932.

---
+++ src/main/java/org/jahia/modules/elasticsearchconnector/rest/ConnectionUnavailableException.java
@@ -0,0 +1,8 @@
+package org.jahia.modules.elasticsearchconnector.rest;
+
+public class ConnectionUnavailableException extends Exception {
+
+    public ConnectionUnavailableException(String message) {
+        super(message);
+    }
+}
