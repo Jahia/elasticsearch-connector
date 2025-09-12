990cf431bc661ca08c7238745d2869805cb83740
Geofrey Flores <gflores@jahia.com>
2025-09-12 11:24:58 -0400

    Migrate config to using OSGi metatype

--- src/main/java/org/jahia/modules/elasticsearchconnector/config/ElasticsearchConfig.java
+++ src/main/java/org/jahia/modules/elasticsearchconnector/config/ElasticsearchConfig.java
@@ -1,109 +1,72 @@
 package org.jahia.modules.elasticsearchconnector.config;
 
-import org.jahia.modules.elasticsearchconnector.ESConstants;
 import org.jahia.modules.elasticsearchconnector.rest.ElasticsearchConnection;
-import org.jahia.services.modulemanager.util.PropertiesList;
-import org.jahia.services.modulemanager.util.PropertiesManager;
-import org.jahia.services.modulemanager.util.PropertiesValues;
-import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
-@Component(service = {ManagedService.class, ElasticsearchConfig.class}, property = {
-        "service.pid=org.jahia.modules.elasticsearch_config",
-        "service.description=Elasticsearch configuration service",
-        "service.vendor=Jahia Solutions Group SA"
-}, immediate = true)
-public class ElasticsearchConfig implements ManagedService {
+@Component(
+        configurationPid = "org.jahia.modules.elasticsearchConnector",
+        service = {ElasticsearchConfig.class},
+        immediate = true,
+        property = {
+                "service.description=Elasticsearch configuration service",
+                "service.vendor=Jahia Solutions Group SA"
+        }
+)
+@Designate(ocd = ElasticsearchConfigMetatype.class)
+public class ElasticsearchConfig {
 
-    private static final String CONFIG_NAMESPACE_PREFIX = "org.jahia.modules.elasticsearch-connector";
-    private static final String PROPERTY_HOST = CONFIG_NAMESPACE_PREFIX + ".host";
-    private static final String PROPERTY_PORT = CONFIG_NAMESPACE_PREFIX + ".port";
-    private static final String PROPERTY_USER = CONFIG_NAMESPACE_PREFIX + ".user";
-    private static final String PROPERTY_PASSWORD = CONFIG_NAMESPACE_PREFIX + ".password";
-    private static final String PROPERTY_USE_XPACK_SECURITY = CONFIG_NAMESPACE_PREFIX + ".useXPackSecurity";
-    private static final String PROPERTY_USE_ENCRYPTION = CONFIG_NAMESPACE_PREFIX + ".useEncryption";
-    private static final String PROPERTY_NODES_SNIFFER_INTERVAL = CONFIG_NAMESPACE_PREFIX + ".nodesSnifferInterval";
-    private static final String PROPERTY_ADDITIONAL_HOSTS = CONFIG_NAMESPACE_PREFIX + ".additionalHostAddresses";
+    private final AtomicReference<ElasticsearchConnection> connection = new AtomicReference<>();
 
-    private static final String DEFAULT_HOST = "elasticsearch";
-    private static final int DEFAULT_PORT = 9200;
-    private static final String DEFAULT_ID = ESConstants.DB_ID;
-    private static final boolean DEFAULT_USE_XPACK_SECURITY = false;
-    private static final boolean DEFAULT_USE_ENCRYPTION = false;
-    private static final String DEFAULT_SNIFFER_INTERVAL = "5s";
-    private static final String DEFAULT_USER = "elastic";
-
-    private ElasticsearchConnection connection;
-
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ElasticsearchConfig() {
         super();
     }
 
-    @Override
-    public void updated(Dictionary<String, ?> dictionary) throws ConfigurationException {
-        if (dictionary != null) {
-            PropertiesManager pm = new PropertiesManager(getMap(dictionary));
-            PropertiesValues values = pm.getValues();
-
-            ElasticsearchConnection con = new ElasticsearchConnection(DEFAULT_ID);
-            con.setPort(getPropertyOrDefault(values, PROPERTY_PORT, DEFAULT_PORT, Integer.class));
-            con.setHost(getPropertyOrDefault(values, PROPERTY_HOST, DEFAULT_HOST, String.class));
-            con.setSnifferInterval(getPropertyOrDefault(values, PROPERTY_NODES_SNIFFER_INTERVAL, DEFAULT_SNIFFER_INTERVAL, String.class));
-            con.setUseXPackSecurity(getPropertyOrDefault(values, PROPERTY_USE_XPACK_SECURITY, DEFAULT_USE_XPACK_SECURITY, Boolean.class));
-            con.setUseEncryption(getPropertyOrDefault(values, PROPERTY_USE_ENCRYPTION, DEFAULT_USE_ENCRYPTION, Boolean.class));
-            con.setUser(getPropertyOrDefault(values, PROPERTY_USER, DEFAULT_USER, String.class));
-            con.setPassword(getPropertyOrDefault(values, PROPERTY_PASSWORD, "", String.class));
-            con.setAdditionalHostAddresses(getPropertyOrDefault(values, PROPERTY_ADDITIONAL_HOSTS, new ArrayList<>(), List.class));
-
-            connection = con;
-        }
+    @Deactivate
+    protected void deactivate() {
+        logger.info("Shutting down Elasticsearch connection...");
+        connection.set(null);
     }
 
-    public ElasticsearchConnection getConnection() {
-        return connection;
+    @Activate
+    @Modified
+    protected void activate(ElasticsearchConfigMetatype config) {
+        logger.info("Activating/Updating elasticsearch configuration...");
+        ElasticsearchConnection connConfig = new ElasticsearchConnection();
+        connConfig.setHost(config.elasticsearchConnector_host());
+        connConfig.setPort(config.elasticsearchConnector_port());
+        connConfig.setUser(config.elasticsearchConnector_user());
+        connConfig.setPassword(config.elasticsearchConnector_password());
+        connConfig.setUseXPackSecurity(config.elasticsearchConnector_useXPackSecurity());
+        connConfig.setUseEncryption(config.elasticsearchConnector_useEncryption());
+        connConfig.setSnifferInterval(config.elasticsearchConnector_snifferInterval());
+
+        // Handle additionalHostAddresses as a String array
+        String[] additionalHosts = config.elasticsearchConnector_additionalHostAddresses();
+        if (additionalHosts != null && additionalHosts.length > 0) {
+            List<String> hosts = Arrays.stream(additionalHosts)
+                    .filter(s -> s != null && !s.trim().isEmpty())
+                    .collect(Collectors.toList());
+            connConfig.setAdditionalHostAddresses(hosts);
+        } else {
+            connConfig.setAdditionalHostAddresses(new ArrayList<>());
+        }
+        connection.set(connConfig);
+        logger.info("Elasticsearch connection configuration updated.");
     }
 
-    private static Map<String, String> getMap(Dictionary<String, ?> d) {
-        Map<String, String> m = new HashMap<>();
-        if (d != null) {
-            Enumeration<String> en = d.keys();
-            while (en.hasMoreElements()) {
-                String key = en.nextElement();
-                if (!key.startsWith("felix.") && !key.startsWith("service.")) {
-                    m.put(key, d.get(key).toString());
-                }
-            }
-        }
-        return m;
-    }
-
-    private <T> T getPropertyOrDefault(PropertiesValues values, String propertyName, T defaultValue, Class<T> type) {
-        Object value = null;
-
-        if (values != null) {
-            if (type == String.class) {
-                value = values.getProperty(propertyName);
-            } else if (type == Integer.class) {
-                value = values.getIntegerProperty(propertyName);
-            } else if (type == Boolean.class) {
-                value = values.getBooleanProperty(propertyName);
-            } else if (type == List.class) {
-                PropertiesList list = values.getList(propertyName);
-                if (list != null) {
-                    value = list.getStructuredList().stream()
-                            .map(obj -> Objects.toString(obj, null))
-                            .collect(Collectors.toList());
-                }
-            }
-        }
-
-        return value != null ? type.cast(value) : defaultValue;
+    public ElasticsearchConnection getConnectionConfig() {
+        return connection.get();
     }
 }
--- src/main/java/org/jahia/modules/elasticsearchconnector/rest/ElasticsearchConnection.java
+++ src/main/java/org/jahia/modules/elasticsearchconnector/rest/ElasticsearchConnection.java
@@ -1,7 +1,12 @@
 package org.jahia.modules.elasticsearchconnector.rest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.jahia.utils.EncryptionUtils;
+
 import java.util.List;
 import java.util.Objects;
+
+import static org.jahia.modules.elasticsearchconnector.ESConstants.*;
 
 /**
  * Represents an Elasticsearch connection configuration.
@@ -17,15 +22,19 @@ import java.util.Objects;
  */
 public class ElasticsearchConnection {
 
-    protected String id;
-    protected String host;
-    protected Integer port;
-    protected String user;
-    protected String password;
-    protected String snifferInterval;
-    protected boolean useXPackSecurity;
-    protected boolean useEncryption;
-    protected List<String> additionalHostAddresses;
+    private String id;
+    private String host;
+    private Integer port;
+    private String user;
+    private String password;
+    private String snifferInterval;
+    private boolean useXPackSecurity;
+    private boolean useEncryption;
+    private List<String> additionalHostAddresses;
+
+    public ElasticsearchConnection() {
+        this.id = DB_ID;
+    }
 
     public ElasticsearchConnection(String id) {
         this.id = id;
@@ -39,48 +48,61 @@ public class ElasticsearchConnection {
         this.id = id;
     }
 
+    public void setHost(String host) {
+        this.host = (host == null || host.trim().isEmpty()) ? DEFAULT_HOST : host;
+    }
+
     public String getHost() {
         return this.host;
     }
 
-    public void setHost(String host) {
-        this.host = host;
+    public void setPort(Integer port) {
+        this.port = (port == null) ? DEFAULT_PORT : port;
     }
 
     public Integer getPort() {
         return this.port;
     }
 
-    public void setPort(Integer port) {
-        this.port = port;
+    public void setUser(String user) {
+        this.user = (user == null || user.trim().isEmpty()) ? DEFAULT_USER : user;
     }
 
     public String getUser() {
         return this.user;
     }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
-
     public String getPassword() {
         return this.password;
+    }
+
+    public char[] decodePassword() {
+        return EncryptionUtils.passwordBaseDecrypt(this.password).toCharArray();
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getSnifferInterval() {
-        return snifferInterval;
+    public boolean hasCredentials() {
+        return StringUtils.isNotEmpty(this.user) && StringUtils.isNotEmpty(this.password);
     }
 
     public void setSnifferInterval(String snifferInterval) {
-        this.snifferInterval = snifferInterval;
+        this.snifferInterval = (snifferInterval == null || snifferInterval.trim().isEmpty()) ? DEFAULT_SNIFFER_INTERVAL : snifferInterval;
+    }
+
+    public String getSnifferInterval() {
+        return this.snifferInterval;
+    }
+
+    public int getSnifferIntervalMillis() {
+        String interval = getSnifferInterval();
+        return (interval != null) ? Integer.parseInt(interval.replaceAll("[^0-9]", "")) * 1000 : -1;
     }
 
     public boolean isUseXPackSecurity() {
-        return useXPackSecurity;
+        return this.useXPackSecurity;
     }
 
     public void setUseXPackSecurity(boolean useXPackSecurity) {
@@ -88,7 +110,15 @@ public class ElasticsearchConnection {
     }
 
     public boolean isUseEncryption() {
-        return useEncryption;
+        return this.useEncryption;
+    }
+
+    public String getProtocolScheme() {
+        return isUseEncryption() ? "https" : "http";
+    }
+
+    public boolean isHttp() {
+        return "http".equalsIgnoreCase(getProtocolScheme());
     }
 
     public void setUseEncryption(boolean useEncryption) {
@@ -96,7 +126,7 @@ public class ElasticsearchConnection {
     }
 
     public List<String> getAdditionalHostAddresses() {
-        return additionalHostAddresses;
+        return this.additionalHostAddresses;
     }
 
     public void setAdditionalHostAddresses(List<String> additionalHostAddresses) {
@@ -112,7 +142,14 @@ public class ElasticsearchConnection {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ElasticsearchConnection that = (ElasticsearchConnection) o;
-        return useXPackSecurity == that.useXPackSecurity && useEncryption == that.useEncryption && Objects.equals(id, that.id) && Objects.equals(host, that.host) && Objects.equals(port, that.port) && Objects.equals(user, that.user) && Objects.equals(password, that.password) && Objects.equals(additionalHostAddresses, that.additionalHostAddresses);
+        return useXPackSecurity == that.useXPackSecurity
+                && useEncryption == that.isUseEncryption()
+                && Objects.equals(id, that.getId())
+                && Objects.equals(host, that.getHost())
+                && Objects.equals(port, that.getPort())
+                && Objects.equals(user, that.getUser())
+                && Objects.equals(password, that.getPassword())
+                && Objects.equals(additionalHostAddresses, that.getAdditionalHostAddresses());
     }
 
     @Override
